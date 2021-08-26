2dcdd8d886579b78b866035bfc4067c5f4c30689
Eugene <eugene.kotsarev@gmail.com>
2021-08-26 15:03:57 +0300

    fix: safari issue with gooey effect, upd: bundler, deps

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,46 @@
-import {interpret} from "xstate";
-import {toggleMachine} from "./scripts/state";
+import { interpret } from 'xstate';
+import { toggleMachine } from './scripts/state';
 
 // Anime
-import anime from "animejs/lib/anime.es.js";
+import anime from 'animejs/lib/anime.es.js';
 
 // Vars
 const button = <HTMLInputElement>document.querySelector('#addNote')
 const toggleService = interpret(toggleMachine);
 
-const toggle = () => {
+const toggle = (): void => {
   button.onclick = () => toggleService.send('TOGGLE')
 };
 
-const buttonDisabled = (btnStatus: boolean) => {
+const buttonDisabled = (btnStatus: boolean): void => {
   button.disabled = btnStatus
 };
 
-const animate = (status: any) => {
+const animate = (status: any): void => {
   const tl = anime.timeline();
-  if (status === "active") {
-    buttonDisabled(true);
-    tl.add({
-      targets: button,
-      translateY: [0, -12, 0],
-      scale: [1, 0.85, 1],
-      rotate: 316,
-      duration: 600,
-      easing: "easeInOutSine",
-    })
-      .add(
-        {
-          targets: ".note-selectors .first",
+
+  switch (status) {
+    case 'active':
+      buttonDisabled(true);
+      tl
+        .add({
+          targets: button,
+          translateY: [0, -12, 0],
+          scale: [1, 0.85, 1],
+          rotate: 316,
+          duration: 600,
+          easing: 'easeInOutSine',
+        })
+        .add({
+          targets: '.note-selectors .first',
           translateY: [0, 80],
           duration: 3200,
           scaleY: [1.8, 1],
-        },
-        "-=400"
-      )
-      .add(
-        {
-          targets: ".note-selectors .other",
+        }, '-=400')
+        .add({
+          targets: '.note-selectors .other',
           translateY: function (el) {
-            return [el.getAttribute("data-from"), el.getAttribute("data-to")];
+            return [el.getAttribute('data-from'), el.getAttribute('data-to')];
           },
           scaleY: [0, 1],
           duration: 1600,
@@ -53,35 +52,36 @@ const animate = (status: any) => {
           complete: function () {
             buttonDisabled(false);
           },
-        },
-        "-=2600"
-      );
-  } else if (status === "inactive") {
-    buttonDisabled(true);
-    tl.add({
-      targets: button,
-      rotate: 0,
-      duration: 600,
-      easing: "easeInOutSine",
-    }).add(
-      {
-        targets: ".note-selectors .selector",
-        translateY: function (el) {
-          return [el.getAttribute("data-to"), 0];
-        },
-        duration: 400,
-        delay: anime.stagger(60),
-        easing: "easeInOutSine",
-        complete: function () {
-          buttonDisabled(false);
-        },
-      },
-      "-=400"
-    );
+        }, '-=2600');
+      break;
+    case 'inactive':
+      buttonDisabled(true);
+      tl
+        .add({
+          targets: button,
+          rotate: 0,
+          duration: 600,
+          easing: 'easeInOutSine',
+        })
+        .add({
+          targets: '.note-selectors .selector',
+          translateY: function (el) {
+            return [el.getAttribute('data-to'), 0];
+          },
+          duration: 400,
+          delay: anime.stagger(60),
+          easing: 'easeInOutSine',
+          complete: function () {
+            buttonDisabled(false);
+          },
+        }, '-=400');
+      break;
+    default:
+      break;
   }
 };
 
-const init = () => {
+const init = (): void => {
   toggleService
     .onTransition((state) => {
       console.log(state.value);
